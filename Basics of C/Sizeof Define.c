a86f58ee170ad1406537e561a8e4137df5a33de6
jakirparvejj <jakirparvej.edu@gmail.com>
2024-10-30 15:47:31 +0600

    first commit

---
+++ Basics of C/Sizeof Define.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main ()
+{
+  int i;
+  float f;
+  double d;
+  char c;
+
+  printf("Size of int: %d Byte \n",sizeof (i));
+  printf("Size of float: %d Byte \n", sizeof (f));
+  printf("Size of double: %d Byte \n", sizeof (d));
+  printf("Size of Char: %d Byte", sizeof (c));
+    return  0;
+}
