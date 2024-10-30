8de414358a860ed0688f7bdc5c51d89a1366b50c
jakirparvejj <jakirparvej.edu@gmail.com>
2024-10-30 16:08:45 +0600

    ASCII

---
+++ Basics of C/ASCII Value to Character.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main ()
+{
+    int n;
+
+    printf("Enter a ASCII Value: ");
+
+    scanf("%d",&n);
+
+    printf("The ASCII character is: %c",n);
+
+
+
+    return  0;
+}
