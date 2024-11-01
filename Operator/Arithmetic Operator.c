7e7de0f1ff6e6a79c3a39f30e14bd3bb2737ebff
jakirparvejj <jakirparvej.edu@gmail.com>
2024-11-01 15:57:43 +0600

    Arithmetic

---
+++ Operator/Arithmetic Operator.c
@@ -0,0 +1,17 @@
+//Two Number add
+
+#include <stdio.h>
+int main ()
+{
+    int num1,num2,sum;
+
+    printf("Enter two Number: ");
+    scanf("%d %d",&num1,&num2);
+
+    sum= num1+num2;
+
+    printf("The sum is: %d\n",sum);
+
+
+    return  0;
+}
