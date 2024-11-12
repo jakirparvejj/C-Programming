0b38d56e5caac52b4e72c4caa890a440fb4f89ca
jakirparvejj <jakirparvej.edu@gmail.com>
2024-11-13 02:53:04 +0600

    Some file Added

---
+++ Operator/Arithmetic Operator/swap_two_number.c
@@ -0,0 +1,18 @@
+// Write a program that two number swap
+
+#include <stdio.h>
+int main ()
+{
+int num1=15;
+int num2=4;
+int temp;
+
+temp=num1;
+num1=num2;
+num2=temp;
+
+printf("Num1= %d\n",num1);
+printf("Num2= %d\n",num2);
+
+    return  0;
+}
